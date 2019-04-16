1a17475f14bf212a32468be8aece37015eeb2e4e
codeWonderland <easter@champlain.edu>
2019-04-16 14:39:33 -0400

    Initial Commit

---
+++ ChitChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChitChat
+//
+//  Created by Easter, Alice on 4/16/19.
+//  Copyright © 2019 Easter, Alice. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ ChitChatTests/ChitChatTests.swift
@@ -0,0 +1,34 @@
+//
+//  ChitChatTests.swift
+//  ChitChatTests
+//
+//  Created by Easter, Alice on 4/16/19.
+//  Copyright © 2019 Easter, Alice. All rights reserved.
+//
+
+import XCTest
+@testable import ChitChat
+
+class ChitChatTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
