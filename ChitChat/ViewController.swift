d84a9e3bac2ed8aafeb9ed02d0f3fe0824e2f373
Fortin <shawn.fortin@mymail.champlain.edu>
2019-04-24 20:06:31 -0400

    sending location info with messages

--- ChitChat/ViewController.swift
+++ ChitChat/ViewController.swift
@@ -8,14 +8,16 @@
 
 import UIKit
 import Alamofire
+import CoreLocation
 
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, CLLocationManagerDelegate {
     
     @IBOutlet weak var table: UITableView!
     var mMessages: [Message] = []
     var mLikeIds: [String] = []
     @IBOutlet weak var mMessageField: UITextField!
     let refresh = UIRefreshControl()
+    let locationManager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,6 +28,12 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         refresh.addTarget(self, action: #selector(handleRefresh), for: .valueChanged)
         self.table.addSubview(self.refresh)
         mLikeIds = UserDefaults.standard.object(forKey: "liked") as? [String] ?? [String]()
+        self.locationManager.requestAlwaysAuthorization()
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.startUpdatingLocation()
+        }
         getMessages()
     }
     
@@ -38,7 +46,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         netRequest(
             path: "",
             method: .get,
-            message: nil
+            message: nil, parameters: ""
         ) { response in
             if let json = response.result.value as? [String: Any],
                 let messages = json["messages"] as? [[String: Any]] {
@@ -54,11 +62,12 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     func sendMessage(message: String) {
         let escapedMessage = message.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed)
+        let location = locationManager.location?.coordinate
         
         netRequest(
             path: "",
             method: .post,
-            message: escapedMessage
+            message: escapedMessage, parameters: "&lat=\(location?.latitude as? Double ?? 0.0)&lon=\(location?.longitude as? Double ?? 0.0)"
         ) { response in
             if let json = response.result.value as? [String: Any],
                 let resp = json["code"] as? Int {
@@ -71,7 +80,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     func likeMessage(message: Message, good: Bool) {
         if message.like(good: good) {
-            netRequest(path: "/\(good ? "like" : "dislike")/\(message._id)", method: .get, message: nil) { response in
+            netRequest(path: "/\(good ? "like" : "dislike")/\(message._id)", method: .get, message: nil, parameters: "") { response in
                 if let json = response.result.value as? [String: Any],
                     let resp = json["code"] as? Int {
                     if resp == 1 {
@@ -85,8 +94,9 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     func netRequest(path: String,
                     method: HTTPMethod,
                     message: String?,
+                    parameters: String,
                     cb: @escaping (DataResponse<Any>) -> Void) {
-        var url = "https://www.stepoutnyc.com/chitchat\(path)?key=c9be0e9b-6265-41c6-9090-fd83f6e4537e&client=shawn.fortin@mymail.champlain.edu"
+        var url = "https://www.stepoutnyc.com/chitchat\(path)?key=c9be0e9b-6265-41c6-9090-fd83f6e4537e&client=shawn.fortin@mymail.champlain.edu\(parameters)"
         
         if message != nil {
             url = "\(url)&message=\(message!)"
